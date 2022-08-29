25b30cb8b3cf246b74374a17abb3141b9b1bcd14
MathiasGenibrel <mathias.geni@gmail.com>
2022-08-29 23:32:25 +0200

    ✨ Add Header
    - Add Header component
    - Add styles
    - Add static folder

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.static("./public"));
+
 app.get("/", (_req, res) => {
   res.render("index", { title: "Hello World!" });
+});
+
+app.get("/createAccount", (_req, res) => {
+  res.render("createAccount", { title: "Hello World!" });
 });
 
 app.listen(3000);
