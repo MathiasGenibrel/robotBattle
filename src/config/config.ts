81e0b8d7c4aaaadac4b2dff76e64ee1ec7bf573b
MathiasGenibrel <mathias.geni@gmail.com>
2022-09-01 20:03:15 +0200

    🐛 Fix config of dotenv
    - Pass the good path for the dotenv config.
    - Add saltRound

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../environment/.env" });
+dotenv.config({ path: "./src/environment/.env" });
 
 export const config = {
+  environment: process.env.NODE_ENV,
   database: {
     host: "localhost",
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || "root",
     schema: process.env.DB_SCHEMA || "roboit",
   },
+  saltRounds: 10, // Use for crypt password (bcrypt dependency)
 };
